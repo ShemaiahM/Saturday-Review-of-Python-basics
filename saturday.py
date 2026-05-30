ec52471d67a11224d6b00923fd147bbef269b522
Shemaiah M <shemkmmillard@gmail.com>
2026-05-30 12:51:49 +0100

    30/05/2026 First Review of Python Basics

---
+++ saturday.py
@@ -0,0 +1,35 @@
+print("Hello Saturday 30th of May 2026!")
+print("Here ill be doing my weekly saturday review of python I've learnt, a little round up of sorts!")
+
+#i know how to write comments lol
+#variables
+newName= "ben"
+new_name = "john"
+current_age = 33
+currentAge = 34
+newCurrentAge = currentAge #here i put varibale in a variable
+
+kindNewAge = 33.5
+print(kindNewAge)
+#i used three data types strings, floats and integers
+
+input("Hello there, whats your name? ")
+yourName = input("What's your name? ")
+print("well hi there " + yourName) #concatenation
+
+print(newName, " says hello")
+print(newName + "'s current age is ", currentAge, "years old")
+
+#simple arithmetic
+#PEMDAS
+print((3+3)* 2) #parentheses
+print(3**3 +3)#exponent 3 x 3 x 3 + 3 =30
+print(3*4)#multiplication
+print(10//2) #floor rounds to the nearest integer
+print(15/2) #divison leaves you with a float typical of python
+print(4+4) #addition simple
+print(30-10)#subtraction
+print(10-30)#minus/negative number
+
+#first week back on python i will get back into it next week and weekly in more depth!
+
